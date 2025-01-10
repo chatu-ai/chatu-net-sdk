b9257b310b40c1ca7aab44696fda3b13c1ac3c3b
Zou Jian <chsword@126.com>
2025-01-10 12:50:19 +0800

    refactor: 优化代码和项目配置
    1. 在 `ChatUAIClient.cs` 文件中，移除了不必要的 `using` 语句，仅保留了 `System.Text`。
    2. 在 `ChatUAISDK.csproj` 文件中，添加了 `<Nullable>enable</Nullable>` 和 `<ImplicitUsings>enable</ImplicitUsings>` 属性，以启用可空引用类型和隐式 `using` 指令。

--- src/ChatUAISDK/ChatUAIClient.cs
+++ src/ChatUAISDK/ChatUAIClient.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using ChatUAISDK.Requests;
 using ChatUAISDK.Responses;
 using Newtonsoft.Json;
